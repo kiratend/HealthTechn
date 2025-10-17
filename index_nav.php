10287cb15ca449be1effb8f3ecdd8f70ed1c3b5b
John Daniel Rosales <dr7541813@gmail.com>
2025-10-17 14:01:23 +0800

    Files

---
+++ index_nav.php
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+  <link href="index.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.html"><i class="fas fa-stethoscope me-2"></i>Med<span>Ventory</span></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link active" href="index.html"><i class="fas fa-home me-1"></i>Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="supply.html"><i class="fas fa-boxes me-1"></i>Supply</a></li>
+          <li class="nav-item"><a class="nav-link" href="contact.php"><i class="fas fa-phone me-1"></i>Contact</a></li>
+          <li class="nav-item"><a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt me-1"></i>Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  </body>
